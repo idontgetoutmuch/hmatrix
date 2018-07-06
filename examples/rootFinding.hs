7c89647cf4320584562c1185acc14ef1a1fccef6
Dominic Steinitz <dominic@steinitz.org>
2018-07-06 15:15:13 +0100

    Almost handle root finding / events

---
+++ examples/rootFinding.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS_GHC -Wall #-}
+
+{-# LANGUAGE NegativeLiterals #-}
+
+import           Numeric.Sundials.CVode.ODE
+import           Numeric.Sundials.ODEOpts (ODEOpts(..), SundialsDiagnostics)
+import           Numeric.LinearAlgebra
+
+
+roberts :: Double -> Vector Double -> Vector Double
+roberts t v = vector $ robertsAux t (toList v)
+  where
+    robertsAux _ [y1, y2, y3] = [ -0.04 * y1 + 1.0e4 * y2 * y3
+                         , 0.04 * y1 - 1.0e4 * y2 * y3 - 3.0e7 * (y2)^(2 :: Int)
+                         , 3.0e7 * (y2)^(2 :: Int)
+                         ]
+    robertsAux _ _ = error "roberts RHS not defined"
+
+rootFn :: Double -> Vector Double -> Vector Double
+rootFn _ v = case xs of
+               [y1, _y2, y3] -> vector [ y1 - 0.0001
+                                       , y3 - 0.01
+                                       ]
+               _             -> error "roberts root function RHS not defined"
+  where
+    xs = toList v
+
+ts :: [Double]
+ts = take 12 $ map (* 10.0) (0.4 : ts)
+
+-- foo :: Either (Matrix Double, Int)
+--               (Matrix Double, SundialsDiagnostics)
+foo = odeSolveVWith'' opts BDF (ScXX' 1.0 1.0e-4 1.0 1.0 (vector [1.0e-8, 1.0e-14, 1.0e-6]))
+                      Nothing roberts (vector [1.0, 0.0, 0.0])
+                      2 rootFn
+                      (vector ts)
+  where
+    opts = ODEOpts { maxNumSteps = 10000
+                   , minStep     = 1.0e-12
+                   , relTol      = 1.0e-4
+                   , absTols     = vector [1.0e-8, 1.0e-14, 1.0e-6]
+                   , initStep    = Nothing
+                   , maxFail     = 10
+                   }
+
+main :: IO ()
+main = putStrLn $ show foo
+
